12930db1d641ec9da345542d16984e0617a29ba5
MlinaricsBenedek <144675299+MlinaricsBenedek@users.noreply.github.com>
2024-09-30 16:05:55 +0200

    PageNotFound, szorgalmi

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import Navbar from "./components/navbar/Navbar";
 import HomePage from "./pages/HomePage/HomePage";
 import UsersPage from "./pages/UsersPage/UsersPage";
 import BadgesPage from "./pages/BadgesPage/BadgesPage";
-
+import PageNotFound from "./pages/PageNotFound/PageNotFound";
 function App() {
   return (
     <div className="App">
@@ -17,6 +17,7 @@ function App() {
         <Route path="home" element={<HomePage />} />
         <Route path="users" element={<UsersPage />} />
         <Route path="badges" element={<BadgesPage />} />
+        <Route path="*" element={<PageNotFound/>}/>
       </Routes>
     </div>
   );
---
+++ src/pages/PageNotFound/PageNotFound.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Page } from "../../components/page/Page";
+const PageNotFound=()=>{
+    return(
+        <Page title="NotFoundPage">
+            <div>Page Not Found</div>
+        </Page>    
+    )
+}
+export default PageNotFound;
